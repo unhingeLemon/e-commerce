a546a1e9142e9f1787e930779b89759b73af3a8e
Mark Capili <angelocapili44@gmail.com>
2020-10-31 20:58:13 +0800

    colors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 // if using ES module add .js
 // and use import syntax
 // also, add "type": "module" at package.json
 import dotenv from 'dotenv';
+import colors from 'colors';
+
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -18,5 +22,7 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`The server is running in ${process.env.NODE_ENV} on ${PORT}`)
+  console.log(
+    `The server is running in ${process.env.NODE_ENV} on ${PORT}`.yellow.bold
+  )
 );
