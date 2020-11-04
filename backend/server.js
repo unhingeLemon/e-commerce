cb5355c3e232b4e0b5ef22fff9261f206cf3a80e
Mark Capili <angelocapili44@gmail.com>
2020-11-04 17:51:42 +0800

    redux

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
 import express from 'express';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 // if using ES module add .js
 // and use import syntax
 // also, add "type": "module" at package.json
 import dotenv from 'dotenv';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
+app.get('/', (req, res) => {
+  res.send('API is running....');
 });
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(
   PORT,
   console.log(
