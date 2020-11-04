e4ff539bb75d612e18d8ca7a62c1a6685c1e88b5
Mark Capili <angelocapili44@gmail.com>
2020-11-04 18:36:37 +0800

    redux on homescreen

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -4,7 +4,12 @@ import {
   PRODUCT_LIST_FAIL,
 } from '../constants/productConstants';
 
-export const productListReducer = (state = { products: [] }, action) => {
+export const productListReducer = (
+  state = {
+    products: [],
+  },
+  action
+) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
